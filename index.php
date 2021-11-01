651eae86457e3d70ff43a05c92bf32e2d3d63a4f
Lucas Lin <lucas@MacBook-Air.local>
2021-11-01 01:00:00 -0400

    Testing

--- index.php
+++ index.php
@@ -86,15 +86,12 @@ $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
-echo "\n\n\n\ db follows ";
-echo $db;
 
 $conn = new mysqli($server, $username, $password, $db);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "\n\nConnected successfully";
 
 $sql = "select * from movies";
 $result = $conn->query($sql);
